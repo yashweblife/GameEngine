b44a6c39e2f6b4ec64cf567f9b7ca87b19494bfe
yashweblife <yashweblife20@gmail.com>
2023-02-08 18:24:17 -0600

    add comments

--- src/lib/Vector/Vector.ts
+++ src/lib/Vector/Vector.ts
@@ -1,3 +1,11 @@
+/**
+ * ## Vector
+ * This is a cartesian point object. It stores 3 dimensional values.
+ * Use `VectorMath` library to do operations.
+ * ```
+ * import {VectorMath} from "./Vector"
+ * ```
+ */
 class Vector {
   public x: number;
   public y: number;
