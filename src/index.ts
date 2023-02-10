69aa9dd7a6fd8e617aec1b6a07950171be517708
yashweblife <yashweblife20@gmail.com>
2023-02-10 16:10:38 -0600

    new test

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { BodyConstructor } from "./lib/RigidBody/BodyConstructor";
 
 const b = new BodyConstructor();
+b.update();
+document.querySelector("#show").addEventListener("click",()=>{
+    b.enableBoundingBox();
+})
