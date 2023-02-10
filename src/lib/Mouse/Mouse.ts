326d05097b6b70002591c47b255a8e28db5239c9
yashweblife <yashweblife20@gmail.com>
2023-02-10 16:09:48 -0600

    feature/add events and tooltip

--- src/lib/Mouse/Mouse.ts
+++ src/lib/Mouse/Mouse.ts
@@ -1,20 +1,49 @@
 import { Vector } from "../Vector";
 export class Mouse {
-  pos: Vector;
-  click: boolean;
-  parent: HTMLElement;
+  public pos: Vector = new Vector(0, 0, 0);
+  public click: boolean = false;
+  public parent: HTMLElement;
+  public tooltip: HTMLElement | null = null;
   constructor(parent: HTMLElement = document.body) {
     this.parent = parent;
     const bound = this.parent.getBoundingClientRect();
-    parent.addEventListener("mousemove", (e: MouseEvent) => {
+    this.parent.addEventListener("mousemove", (e: MouseEvent) => {
       this.pos.x = e.clientX - bound.left;
       this.pos.y = e.clientY - bound.top;
+      if (this.tooltip !== null) {
+        this.tooltip.style.top = `${this.pos.y-50}px`;
+        this.tooltip.style.left = `${this.pos.x-20}px`;
+        this.tooltip.innerHTML = `X:${this.pos.x}px | Y:${this.pos.y}px`;
+      }
     });
-    parent.addEventListener("mousedown", (e: MouseEvent) => {
+    this.parent.addEventListener("mousedown", (e: MouseEvent) => {
       this.click = true;
     });
-    parent.addEventListener("mouseup", (e: MouseEvent) => {
+    this.parent.addEventListener("mouseup", (e: MouseEvent) => {
       this.click = false;
     });
   }
+  public onMove = (func: Function) => {
+    this.parent.addEventListener("mousemove", (e: MouseEvent) => {
+      func(this.pos);
+    });
+  };
+  public onClick = (func: Function) => {
+    this.parent.addEventListener("mouseup", (e: MouseEvent) => {
+      func(new Vector(this.pos.x, this.pos.y, this.pos.z));
+    });
+  };
+  public enableToolTip = () => {
+    this.tooltip = document.createElement("div");
+    this.tooltip.style.position = "absolute";
+    this.tooltip.style.zIndex = "100";
+    this.tooltip.style.backgroundColor = "rgba(32,32,32,0.4)";
+    this.tooltip.style.color="white";
+    this.tooltip.style.padding="0.5em";
+    this.tooltip.style.borderRadius="1em";
+    this.parent.parentElement.append(this.tooltip)
+  };
+  public disableToolTip = () => {
+    this.tooltip = null;
+  };
 }
