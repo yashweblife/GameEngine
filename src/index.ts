74dc9bbfd066f25f54a8dedbb2c5f58db75e581e
yashweblife <yashweblife20@gmail.com>
2023-02-11 01:56:26 -0600

    reset index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,13 @@
-import { BodyConstructor } from "./lib/RigidBody/BodyConstructor";
+import { Canvas } from "./lib/Canvas/Canvas";
+import { Body } from "./lib/RigidBody/Body";
+import { Vector } from "./lib/Vector";
+const canvas = new Canvas();
+const test: Vector[] = [
+  new Vector(-0.9643516153778124, 0.2646241899717482, 0),
+  new Vector(0.03230252534863266, -0.9994781372576895, 0),
+  new Vector(0.6510875613551799, 0.7590026267731654, 0),
+  new Vector(-0.9643516153778124, 0.2646241899717482, 0),
+];
 
-const b = new BodyConstructor();
-b.update();
-document.querySelector("#showBB").addEventListener("click", () => {
-  b.enableBoundingBox();
-});
-document.querySelector("#showCM").addEventListener("click", () => {
-  b.enableCenterOfMass();
-});
-
-document.querySelector("#showEB").addEventListener("click", () => {
-  b.enableEdgeBisector();
-});
-document.querySelector("#showNOR").addEventListener("click", () => {
-  b.enableNormalize();
-});
-document.querySelector("#scaler").addEventListener("input", () => {
-  let val = document.querySelector("#scaler") as HTMLInputElement;
-  b.setScale(Number(val.value));
-});
+const bod: Body = new Body(test);
+bod.show(canvas);
