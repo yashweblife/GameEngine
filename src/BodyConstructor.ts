3a95513e0d197f3f5af3b4dd745fc6b0d6fc10ff
yashweblife <yashweblife20@gmail.com>
2023-02-11 01:56:43 -0600

    add base code

---
+++ src/BodyConstructor.ts
@@ -0,0 +1,24 @@
+import { BodyConstructor } from "./lib/RigidBody/BodyConstructor";
+
+const b = new BodyConstructor();
+b.update();
+document.querySelector("#showBB").addEventListener("click", () => {
+  b.enableBoundingBox();
+});
+document.querySelector("#showCM").addEventListener("click", () => {
+  b.enableCenterOfMass();
+});
+
+document.querySelector("#showEB").addEventListener("click", () => {
+  b.enableEdgeBisector();
+});
+document.querySelector("#showNOR").addEventListener("click", () => {
+  b.enableNormalize();
+});
+document.querySelector("#scaler").addEventListener("input", () => {
+  let val = document.querySelector("#scaler") as HTMLInputElement;
+  b.setScale(Number(val.value));
+});
+document.querySelector("#generate").addEventListener("click", () => {
+  b.generateBody();
+});
