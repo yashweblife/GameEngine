1df922700c33a3df32c0b4280dccbcb1e039cd4f
yashweblife <yashweblife20@gmail.com>
2023-02-16 00:58:37 -0600

    add other vectors

--- src/lib/Vector/index.ts
+++ src/lib/Vector/index.ts
@@ -1,4 +1,7 @@
+import { Vec2d } from "./Vec2d";
+import { Vec3d } from "./Vec3d";
+import { Vec4d } from "./Vec4d";
 import { Vector } from "./Vector";
 import { VectorMath } from "./VectorMath";
-export { Vector, VectorMath };
+export { Vector, VectorMath, Vec2d, Vec3d, Vec4d };
 
